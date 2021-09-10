1f75247ca9862cb49a83ebfb6d0f967800771aac
abhaysingh00 <abhay.2024cse1147@kiet.edu>
2021-09-10 23:55:35 +0530

    print position of the numbers

--- max and secondMax.py
+++ max and secondMax.py
@@ -7,13 +7,16 @@ while(i<=n):
     num=int(input())
     l.append(num)
     i=i+1
-for j in l:
-    if j>max1:
+for j in range(n):
+    if l[j]>max1:
         max2=max1
-        max1=j
-    elif j>max2:
-        max2=j
-print("largest number is",max1)
-print("second largest number is",max2)
+        max1=l[j]
+        pos1=j+1
+        
+    elif l[j]>max2:
+        max2=l[j]
+        pos2=j+1
+print("largest number is",max1, "at position",pos1)
+print("second largest number is",max2, "at position", pos2)
     
     
